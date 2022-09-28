d2722aa079bc7478142e60376629aaae58252bcc
Andres Godoy <andresegm@gmail.com>
2022-09-27 22:57:24 -0600

    Refractor code to make player selection using buttons. Add container division for scores

--- script.js
+++ script.js
@@ -1,59 +1,75 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const container = document.querySelector('#container');
+const content = document.createElement('div')
+container.appendChild(content);
+content.textContent = `Player's Score: ${playerScore}. Computer's score: ${computerScore}`;
+
 const getComputerChoice = () => {
     let computerChoice = "";
     let randomNum = Math.floor(Math.random() * 3);
     if (randomNum === 0) {
-        computerChoice = 'Rock';
+        computerChoice = 'rock';
     } else if (randomNum === 1) {
-        computerChoice = 'Paper';
+        computerChoice = 'paper';
     } else if (randomNum === 2) {
-        computerChoice = 'Scissors';
+        computerChoice = 'scissors';
     }
     return computerChoice;
 }
 
-let playerScore = 0;
-let computerScore = 0;
+const updateScore = async () => {
+    content.textContent = `Player's Score: ${playerScore}. Computer's score: ${computerScore}`;
+    if (playerScore === 5) {
+        await playRound();
+        console.log('Player wins the match!!');
+    } else if (computerScore === 5) {
+        await playRound();
+        console.log('Computer wins the match!!');
+    }
+}
 
 const playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-   if (playerSelection === computerSelection) {
+    if (playerScore === 5 || computerScore === 5) {
+        return 'This game is over. Please refresh the page to play again'
+        }
+    if (playerSelection === computerSelection) {
+            updateScore();
             return `You chose: ${playerSelection}. The computer chose: ${computerSelection}. It is a tie!!`
         } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            computerScore++
+            computerScore++;
+            updateScore();
             return `You chose: ${playerSelection}. The computer chose: ${computerSelection}. The computer wins this round!`
         } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            playerScore++
+            playerScore++;
+            updateScore();
             return `You chose: ${playerSelection}. The computer chose: ${computerSelection}. You win this round!`
         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerScore++
+            computerScore++;
+            updateScore();
             return `You chose: ${playerSelection}. The computer chose: ${computerSelection}. The computer wins this round`
         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            playerScore++
+            playerScore++;
+            updateScore();
             return `You chose: ${playerSelection}. The computer chose: ${computerSelection}. You win this round!`
         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerScore++
+            computerScore++;
+            updateScore();
             return `You chose: ${playerSelection}. The computer chose: ${computerSelection}. The computer wins this round!`
         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            playerScore++
+            playerScore++;
+            updateScore();
             return `You chose: ${playerSelection}. The computer chose: ${computerSelection}. You win this round!`
         } else {
             return `Please, make a valid selection`
         }
 }
 
-const playGame = () => {
-    for (let i =0; i < 5; i++) {
-        console.log(playRound(prompt('Make your selection'), getComputerChoice()));
-        console.log(`Your score: ${playerScore}. Computer's Score: ${computerScore}`)
-    }
-    if (playerScore > computerScore) {
-        console.log('You are the winner!!');
-    } else if (playerScore < computerScore) {
-        console.log('The computer triumphs!!');
-    } else {
-        console.log('It seems to be a tie.')
-    }
-}
-
-playGame();
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+    playerSelection = e.target.className;
+    console.log(playRound(playerSelection, getComputerChoice()))
+    });
+});
